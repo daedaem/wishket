a44113fff6de2984060fa1f7f92362bd5e960cd7
daedaem <tg8685@gmail.com>
2023-03-11 00:08:40 +0900

    feat: CompanyJobsPage, 채용 상세 조회 기능 추가

--- src/components/Layout/SideBar/SideBar.jsx
+++ src/components/Layout/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import classes from "./SideBar.module.css";
 const SideBar = (props) => {
+  console.log(props);
   const date = new Date();
   const Dday = new Date(props.value.date_established?.split("-"));
   const establishedDate =
@@ -14,15 +15,15 @@ const SideBar = (props) => {
   const sideBarContet = (
     <dl className={classes.sideBarContent}>
       {/* <div> */}
-      <dt>매출 :</dt>
+      <dt>매 출 :</dt>
       <dd>{props.value.sales}</dd>
       <dt>설립일 :</dt>
       <dd>
         {establishedDate}
         <br />({howLongDate}년차)
       </dd>
-      <dt>사원 수 :</dt> <dd>{props.value.headcount}명</dd>
-      <dt>홈페이지:</dt>
+      <dt>사원수 :</dt> <dd>{props.value.headcount}명</dd>
+      <dt>홈페이지 :</dt>
       <dd>
         <a href={`${props.value.link_url}`} target="_blank" rel="noreferrer">
           바로가기
@@ -30,10 +31,14 @@ const SideBar = (props) => {
       </dd>
     </dl>
   );
+
+  const navigateHandler = () => {
+    navigate("/1");
+  };
   return (
     <aside className={classes.sideBar}>
       <div className={classes.sideBarFrame}>
-        <div className={classes.sideBarHead}>
+        <div className={classes.sideBarHead} onClick={navigateHandler}>
           <img
             className={classes.sideBarHeadImage}
             src={props.value.logo}
